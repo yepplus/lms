cfe0b653303a7a86d21797348740a2fc5052cb69
yepplus <3386477855@qq.com>
2023-12-21 20:50:09 +0800

    添加 Navbar

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Sidebar from './_components/sidebar'
+import { Navbar } from './_components/navbar'
 
 interface LayoutProps {
   children: React.ReactNode
@@ -8,6 +9,9 @@ interface LayoutProps {
 const Layout = ({ children }: LayoutProps) => {
   return (
     <div className="h-full">
+      <div className="h-[80px] md:pl-56 fixed inset-y-0 w-full z-50">
+        <Navbar />
+      </div>
       <div className="hidden md:flex h-full w-56 flex-col fixed inset-y-0 z-50">
         <Sidebar />
       </div>
