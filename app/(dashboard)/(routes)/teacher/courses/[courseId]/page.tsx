e8c7bad032594499b33011af25137934ea3fe894
yepplus <3386477855@qq.com>
2024-01-05 21:25:08 +0800

    添加创建课程表单中的 Course Price

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -2,11 +2,12 @@ import { db } from '@/lib/db'
 import { auth } from '@clerk/nextjs'
 import { redirect } from 'next/navigation'
 import { IconBadge } from '@/components/icon-badge'
-import { LayoutDashboardIcon } from 'lucide-react'
+import { CircleDollarSign, LayoutDashboardIcon, ListChecks } from 'lucide-react'
 import { TitleForm } from './_components/title-form'
 import { DescriptionForm } from './_components/description-form'
 import { ImageForm } from './_components/image-form'
 import { CategoryForm } from './_components/category-form'
+import { PriceForm } from '@/app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/price-form'
 
 const Page = async ({ params }: { params: { courseId: string } }) => {
   const { userId } = auth()
@@ -62,6 +63,22 @@ const Page = async ({ params }: { params: { courseId: string } }) => {
             courseId={course.id}
             options={categories.map((c) => ({ label: c.name, value: c.id }))}
           />
+        </div>
+        <div className="space-y-6">
+          <div>
+            <div className="flex items-center gap-x-2">
+              <IconBadge icon={ListChecks} />
+              <h2 className="text-xl">Course chapters</h2>
+            </div>
+            <div>TODO: Chapters</div>
+          </div>
+          <div>
+            <div className="flex items-center gap-x-2">
+              <IconBadge icon={CircleDollarSign} />
+              <h2 className="text-xl">Sell your course</h2>
+            </div>
+            <PriceForm initialData={course} courseId={course.id} />
+          </div>
         </div>
       </div>
     </div>
