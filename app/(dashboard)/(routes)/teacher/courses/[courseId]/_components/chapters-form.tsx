455fd28ac6574c3cae88f86f62f819ef184c1684
yepplus <3386477855@qq.com>
2024-01-18 22:14:16 +0800

    添加 Chapters 的编辑页面的标题部分

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/chapters-form.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/chapters-form.tsx
@@ -74,6 +74,10 @@ export const ChaptersForm = ({ initialData, courseId }: ChaptersFormProps) => {
     }
   }
 
+  const onEdit = (id: string) => {
+    router.push(`/teacher/courses/${courseId}/chapters/${id}`)
+  }
+
   return (
     <div className="relative mt-6 border bg-slate-100 rounded-md p-4">
       {isUpdating && (
@@ -131,7 +135,7 @@ export const ChaptersForm = ({ initialData, courseId }: ChaptersFormProps) => {
         >
           {!initialData.chapters.length && 'No chapters'}
           <ChaptersList
-            onEdit={() => {}}
+            onEdit={onEdit}
             onReorder={onReorder}
             items={initialData.chapters || []}
           />
