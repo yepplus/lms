d5aa443cd347f0e7b4c1504c30531d762b77b0f3
yepplus <3386477855@qq.com>
2024-01-22 20:13:25 +0800

    添加 Chapters 是否免费的编辑功能

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/_components/chapter-access-form.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/_components/chapter-access-form.tsx
@@ -7,9 +7,9 @@ import { useForm } from 'react-hook-form'
 import {
   Form,
   FormControl,
+  FormDescription,
   FormField,
-  FormItem,
-  FormMessage
+  FormItem
 } from '@/components/ui/form'
 import { Button } from '@/components/ui/button'
 import { Pencil } from 'lucide-react'
@@ -19,31 +19,30 @@ import axios from 'axios'
 import { useRouter } from 'next/navigation'
 import { cn } from '@/lib/utils'
 import { Chapter } from '@prisma/client'
-import { Editor } from '@/components/editor'
-import { Preview } from '@/components/preview'
+import { Checkbox } from '@/components/ui/checkbox'
 
-interface ChapterDescriptionFormProps {
+interface ChapterAccessFormProps {
   initialData: Chapter
   courseId: string
   chapterId: string
 }
 
 const formSchema = z.object({
-  description: z.string().min(1)
+  isFree: z.boolean().default(false)
 })
 
-export const ChapterDescriptionForm = ({
+export const ChapterAccessForm = ({
   initialData,
   courseId,
   chapterId
-}: ChapterDescriptionFormProps) => {
+}: ChapterAccessFormProps) => {
   const [isEditing, setIsEditing] = useState(false)
   const toggleEdit = () => setIsEditing((current) => !current)
   const router = useRouter()
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      description: initialData?.description || ''
+      isFree: !!initialData?.isFree
     }
   })
   const { isSubmitting, isValid } = form.formState
@@ -64,30 +63,31 @@ export const ChapterDescriptionForm = ({
   return (
     <div className="mt-6 border bg-slate-100 rounded-md p-4">
       <div className="font-medium flex items-center justify-between">
-        Chapter description
+        Chapter access
         <Button onClick={toggleEdit} variant="ghost">
           {isEditing ? (
             <>Cancel</>
           ) : (
             <>
               <Pencil className="h-4 w-4 mr-2" />
-              Edit description
+              Edit access
             </>
           )}
         </Button>
       </div>
       {!isEditing ? (
-        <div
+        <p
           className={cn(
             'text-sm mt-2',
-            !initialData.description && 'text-slate-500 italic'
+            !initialData.isFree && 'text-slate-500 italic'
           )}
         >
-          {!initialData.description && 'No description'}
-          {initialData.description && (
-            <Preview value={initialData.description} />
+          {initialData.isFree ? (
+            <>This chapter is free for preview.</>
+          ) : (
+            <>This chapter is not free.</>
           )}
-        </div>
+        </p>
       ) : (
         <Form {...form}>
           <form
@@ -95,14 +95,22 @@ export const ChapterDescriptionForm = ({
             className="space-y-4 mt-4"
           >
             <FormField
-              name="description"
+              name="isFree"
               control={form.control}
               render={({ field }) => (
-                <FormItem>
+                <FormItem className="flex flex-row items-start space-x-3 space-y-0 rounded-md border p-4">
                   <FormControl>
-                    <Editor {...field} />
+                    <Checkbox
+                      checked={field.value}
+                      onCheckedChange={field.onChange}
+                    />
                   </FormControl>
-                  <FormMessage />
+                  <div className="space-y-1 leading-none">
+                    <FormDescription>
+                      Check this box if you want to make this chapter free for
+                      preview
+                    </FormDescription>
+                  </div>
                 </FormItem>
               )}
             />
